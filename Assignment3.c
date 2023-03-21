0f48709f95ec90ad21504b21981e32dc4506180c
priyanshu2210997179 <127485253+priyanshu2210997179@users.noreply.github.com>
2023-03-21 13:37:43 +0530

    Update Assignment3.c

--- Assignment3.c
+++ Assignment3.c
@@ -1,7 +1,18 @@
 //Can you help your digital design faculty in correcting the assignment on number conversions by writing a program to convert a decimal number to its binary equivalent using a while loop.
 //Example, lets say input is 7, output should be 111
 
-int main()
+int decimalnumber, remainder, binarynumber = 0, base = 1;
+
+
+printf("Enter a decimal number: ");
+scanf("%d", &decimalnumber);
+
+while (decimalnumber > 0)
 {
-  return 0;
+    remainder = decimalnumber % 2;
+    binarynumber += remainder * base;
+    base *= 10;
+    decimalnumber /= 2;
 }
+
+printf("The binary equivalent is: %d", binarynumber);

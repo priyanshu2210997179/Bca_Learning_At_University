92368003338f5fc2a1a5ac90e60a0327c0ef7d04
priyanshu2210997179 <127485253+priyanshu2210997179@users.noreply.github.com>
2023-04-27 20:36:26 +0530

    Update Assignment10.c

--- Assignment10.c
+++ Assignment10.c
@@ -3,37 +3,45 @@
 //if all elements in the array are even it is known as "EVEN ARRAY"
 //if both even and odd available it is known as MIXED array
 
+#include <stdio.h>
 
-int findType(int n, int *) //complete this function
-{
-  
-  #include <stdio.h>
+int findType(int *arr, int n) {
+    int oddCount = 0;
+    int evenCount = 0;
 
-int main() {
-    int arr[] = {1, 3, 5, 7}; // Example array, change this as per requirement
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int even = 0, odd = 0;
-
-    for (int i = 0; i < n; i++) {
-        if (arr[i] % 2 == 0) {
-            even++;
-        } else {
-            odd++;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] % 2 == 0) {
+            evenCount++;
+        }
+        else {
+            oddCount++;
         }
     }
 
-    if (even == 0 && odd > 0) {
-        printf("ODD ARRAY\n");
-    } else if (odd == 0 && even > 0) {
+    if(evenCount == n) {
         printf("EVEN ARRAY\n");
-    } else {
-        printf("MIXED ARRAY\n");
     }
+    else if(oddCount == n) {
+        printf("ODD ARRAY\n");
+    }
+    else {
+        printf("MIXED ARRAY\n");
+      }
+}
+
+int main() {
+    int arr1[] = {1,3,5,11,9};
+    int arr2[] = {2,4,6,8,10};
+    int arr3[] = {1,2,3,4,5,6};
+
+    findType(arr1, 5);
+    findType(arr2, 5);
+    findType(arr3, 6);
 
     return 0;
 }
-}
-int main()
-{
-  return 0;
-}
+
+
+
+
+

62af4598c1b90a4c0e5390697653de2b8b5fb7f4
priyanshu2210997179 <127485253+priyanshu2210997179@users.noreply.github.com>
2023-04-27 15:58:44 +0530

    Update Assignment10.c

--- Assignment10.c
+++ Assignment10.c
@@ -7,6 +7,31 @@
 int findType(int n, int *) //complete this function
 {
   
+  #include <stdio.h>
+
+int main() {
+    int arr[] = {1, 3, 5, 7}; // Example array, change this as per requirement
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int even = 0, odd = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 2 == 0) {
+            even++;
+        } else {
+            odd++;
+        }
+    }
+
+    if (even == 0 && odd > 0) {
+        printf("ODD ARRAY\n");
+    } else if (odd == 0 && even > 0) {
+        printf("EVEN ARRAY\n");
+    } else {
+        printf("MIXED ARRAY\n");
+    }
+
+    return 0;
+}
 }
 int main()
 {
